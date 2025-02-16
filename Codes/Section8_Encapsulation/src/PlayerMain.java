ccdc179739d3bb3ebee04d4b4a9649549595066d
I578070 <mohit.naskar@sap.com>
2025-02-16 21:57:05 +0530

    Push for Section 8 Encapsulation

---
+++ Codes/Section8_Encapsulation/src/PlayerMain.java
@@ -0,0 +1,14 @@
+public class PlayerMain {
+    public static void main(String[] args) {
+//        Player player = new Player();
+//        player.name = "Mohit";
+//        player.health = 54;
+//        player.weapon = "sword";
+//        int damage = 52;
+//        int healthRestored = 100;
+//        //player.loseHealth(damage);
+//        player.restoreHealth(damage);
+        EnhancedPlayer tim = new EnhancedPlayer("Tim",200,"Sword");
+
+    }
+}
